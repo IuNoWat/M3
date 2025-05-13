93c3427b79ddf2eb133c32ddbcbb08515c733cf0
IuNoWat <lorenzo.jacques@alsace.eu>
2025-05-13 15:44:39 +0100

    Version fonctionnelle

--- arduino_serial.py
+++ arduino_serial.py
@@ -1,24 +1,38 @@
+#!/usr/bin/python
+#coding: utf-8
 
 import serial
 import time
+from threading import Thread
 
-arduino=serial.Serial("/dev/ttyACM0",9600,timeout=1)
+class Arduino(Thread) :
+    def __init__(self) :
+        Thread.__init__(self)
+        self.api=serial.Serial("/dev/ttyACM0",9600,timeout=1)
+        self.on=True
+        self.unicode_error=0
+        self.msg="coucou"
+    def run(self) :
+        while self.on :
+            try :
+                self.msg=self.api.read_until().decode()
+            except UnicodeDecodeError :
+                self.unicode_error+=1
+    def get_msg(self) :
+        to_return=self.msg[:7]
+        if len(to_return)<6 :
+            print("LE MESSAGE DE L'ARDUINO N'EST PAS ASSEZ LONG : ")
+            print(self.msg)
+            return "666666"
+        return to_return
 
-def get_arduino_info() :
-    last_msg=arduino.readline()
-    to_return=[
-        last_msg[4:5],
-        last_msg[12:13],
-        last_msg[20:21],
-        last_msg[28:29],
-        last_msg[36:37],
-        last_msg[44:45]
-    ]
-    arduino.flushInput()
-    return to_return
 
 if __name__=="__main__" :
     while True :
-        print(f"Serial message : {arduino.readline()}")
-        print(f"Traduction     : {get_arduino_info()}")
-        time.sleep(0.1)
+        pass
+        rah=Arduino()
+        rah.start()
+        for i in range(0,10) :
+            time.sleep(1)
+            print(rah.get_msg())
+        rah.join()
--- do_the_thing.py
+++ do_the_thing.py
@@ -2,6 +2,7 @@
 #coding: utf-8
 
 import time
+import random
 
 import pygame
 from pygame.locals import *
@@ -14,50 +15,162 @@ import arduino_serial as arduino
 FPS=30
 SCREEN_SIZE=(1600, 900)
 FULLSCREEN=True
+DEBUG=True
 
 #ASSETS
-
+NUM_FONT_PATH="assets/DS-DIGI.TTF"
+TXT_FONT_PATH="assets/Rubik-VariableFont_wght.ttf"
 
 #COLORS
 WHITE=pygame.Color("White")
 BLACK=pygame.Color("Black")
+GREEN=pygame.Color("Green")
+RED=pygame.Color("Red")
 COLOR_BG=pygame.Color(22,13,34,255)
 COLOR_HL=pygame.Color(255,255,255,255)
 
-#POSITION OF NUMBERS
+#STYLE OF NUMBER
+FONT_SIZE=30
+FONT_STYLE=NUM_FONT_PATH
+FONT_COLOR=COLOR_HL
+RESET_ANIMATION_TIME=40
 
+debug_font=pygame.font.Font(TXT_FONT_PATH,16)
+
+#GAMEPLAY
+texts=[
+    "0 kWh",
+    "4 kWh",
+    "25 kWh",
+    "357 kWh",
+    "700 kWh",
+    "2 000 kWh"
+]
+
+#POSITION OF NUMBERS
+number_pos=[
+    (100,450 ),
+    (350,450 ),
+    (600,450 ),
+    (850,450 ),
+    (1100,450),
+    (1350,450),
+]
 
 #NUMBER CLASS
 class Number() :
-    def __init__(self,pos,analogEntry) :
+    def __init__(self,pos,analogEntry,txt_number) :
         self.pos=pos
+        self.current_pos=pos
         self.analogEntry=analogEntry
         self.value=False
+        self.txt_number=txt_number
+        self.status="unconnected"
+        self.woobling_timer=0
+        self.animation_playing=False
         #Style des nombres
-        self.font = pygame.font.Font(NUM_FONT_PATH,font_size)
+        self.font_size=20
+        self.font = pygame.font.Font(NUM_FONT_PATH,FONT_SIZE)
+    def update(self,last_values) :
+        self.value=last_values[self.analogEntry]
+        if self.value=="6" :
+            self.status="unconnected"
+        elif self.value==str(self.txt_number) :
+            self.status=True
+        else :
+            self.status=False
+        
+        if self.woobling_timer>0 :
+            self.woobling_timer-=1
+        if self.woobling_timer==0 :
+            self.animation_playing=False
+
+    def start_reset_anim(self) :
+        if self.animation_playing==False : 
+            self.woobling_timer=RESET_ANIMATION_TIME
+            self.animation_playing=True
     def render(self) :
-        self.value=arduino.get_arduino_info()[self.analogEntry]
-        return self.font.render(formated_value,1,COLOR_HL,COLOR_BG)
+        txt=f"{self.value} - "+texts[self.txt_number]+f" ({self.txt_number})"
 
+        if self.status=="unconnected" :
+            to_return=self.font.render(txt,1,COLOR_HL,COLOR_BG)
+        elif self.status :
+            to_return=self.font.render(txt,1,GREEN,COLOR_BG)
+        else :
+            to_return=self.font.render(txt,1,RED,COLOR_BG)
 
+        if self.animation_playing :
+            self.current_pos=(self.pos[0]+random.randrange(-RESET_ANIMATION_TIME+self.woobling_timer,RESET_ANIMATION_TIME-self.woobling_timer),self.pos[1]+random.randrange(-RESET_ANIMATION_TIME+self.woobling_timer,RESET_ANIMATION_TIME-self.woobling_timer))
+            #elf.current_pos[0]=self.pos[0]+random.randrange(-RESET_ANIMATION_TIME+self.woobling_timer,RESET_ANIMATION_TIME-self.woobling_timer)
+            #elf.current_pos[1]=self.pos[1]+random.randrange(-RESET_ANIMATION_TIME+self.woobling_timer,RESET_ANIMATION_TIME-self.woobling_timer)
+        else :
+            self.current_pos=self.pos
+        return to_return
 
 #MAINLOOP PREPARATION
 running=True
 SCREEN = pygame.display.set_mode(SCREEN_SIZE,pygame.FULLSCREEN)
 CLOCK = pygame.time.Clock()
+NUMBERS=[]
+good_cables=0
+reset_timer=0
+for i,pos in enumerate(number_pos) :
+    NUMBERS.append(Number(pos,i,i))
+
+#Launching thread
+thread=arduino.Arduino()
+thread.start()
 
 #MAINLOOP
 while running :
     #Cleaning of Screen
-    SCREEN.blit
+    SCREEN.fill(COLOR_BG)
 
     #Event handling
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if pygame.mouse.get_pressed() :
-            running = False
+            thread.join()
+        #if pygame.mouse.get_pressed() :
+        #    running = False
+
+    #Value update
+    arduino_values=thread.get_msg()
     
+    #Numbers rendering
+    for number in NUMBERS :
+        number.update(arduino_values)
+        SCREEN.blit(number.render(),number.current_pos)
+    
+    #Victory check
+    good_cables=0
+    for number in NUMBERS :
+        if number.status==True :
+            good_cables+=1
+    
+    if good_cables==6 :
+        for number in NUMBERS :
+            number.start_reset_anim()
+            reset_timer=RESET_ANIMATION_TIME-4
+    
+    if reset_timer>0 :
+        reset_timer-=1
+        if reset_timer==0 :
+            to_give=[0,1,2,3,4,5]
+            for i,number in enumerate(NUMBERS) :
+                new_number=random.choice(to_give)
+                while new_number==number.txt_number :
+                    new_number=random.choice(to_give)
+                number.txt_number=new_number
+                to_give.remove(new_number)
+                
+    #Show FPS
+    if DEBUG :
+        fps = str(round(CLOCK.get_fps(),1))
+        txt = "DEBUG MODE | FPS : "+fps+f" | Data from arduino : {arduino_values} | Error passed in arduino thread : "+str(thread.unicode_error)
+        to_blit=debug_font.render(txt,1,WHITE,COLOR_BG)
+        SCREEN.blit(to_blit,(0,0))
+
     #End of loop
     pygame.display.flip()
     CLOCK.tick(FPS)
