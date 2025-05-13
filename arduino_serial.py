118e4975c6ada5e66f63fb86b1e12d4bdd80c5aa
IuNoWat <lorenzo.jacques@alsace.eu>
2025-05-13 06:20:35 +0100

    ça avance

--- arduino_serial.py
+++ arduino_serial.py
@@ -1,9 +1,24 @@
 
 import serial
+import time
 
 arduino=serial.Serial("/dev/ttyACM0",9600,timeout=1)
 
+def get_arduino_info() :
+    last_msg=arduino.readline()
+    to_return=[
+        last_msg[4:5],
+        last_msg[12:13],
+        last_msg[20:21],
+        last_msg[28:29],
+        last_msg[36:37],
+        last_msg[44:45]
+    ]
+    arduino.flushInput()
+    return to_return
 
 if __name__=="__main__" :
     while True :
-        print(arduino.readline())
+        print(f"Serial message : {arduino.readline()}")
+        print(f"Traduction     : {get_arduino_info()}")
+        time.sleep(0.1)
--- do_the_thing.py
+++ do_the_thing.py
@@ -1,4 +1,64 @@
 #!/usr/bin/python
 #coding: utf-8
 
+import time
+
 import pygame
+from pygame.locals import *
+pygame.init()
+pygame.font.init()
+
+import arduino_serial as arduino
+
+#CONSTANTS
+FPS=30
+SCREEN_SIZE=(1600, 900)
+FULLSCREEN=True
+
+#ASSETS
+
+
+#COLORS
+WHITE=pygame.Color("White")
+BLACK=pygame.Color("Black")
+COLOR_BG=pygame.Color(22,13,34,255)
+COLOR_HL=pygame.Color(255,255,255,255)
+
+#POSITION OF NUMBERS
+
+
+#NUMBER CLASS
+class Number() :
+    def __init__(self,pos,analogEntry) :
+        self.pos=pos
+        self.analogEntry=analogEntry
+        self.value=False
+        #Style des nombres
+        self.font = pygame.font.Font(NUM_FONT_PATH,font_size)
+    def render(self) :
+        self.value=arduino.get_arduino_info()[self.analogEntry]
+        return self.font.render(formated_value,1,COLOR_HL,COLOR_BG)
+
+
+
+#MAINLOOP PREPARATION
+running=True
+SCREEN = pygame.display.set_mode(SCREEN_SIZE,pygame.FULLSCREEN)
+CLOCK = pygame.time.Clock()
+
+#MAINLOOP
+while running :
+    #Cleaning of Screen
+    SCREEN.blit
+
+    #Event handling
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if pygame.mouse.get_pressed() :
+            running = False
+    
+    #End of loop
+    pygame.display.flip()
+    CLOCK.tick(FPS)
+
